d4f0523cd6e8225343fa7e75effe67addd37c21e
Vinicius Azevedo <Viniciuslldeazevedo@gmail.com>
2022-12-04 21:59:49 -0300

    Metodos

--- anatomia-classes/src/edu/vinicius/modulo/classe/MinhaClasse.java
+++ anatomia-classes/src/edu/vinicius/modulo/classe/MinhaClasse.java
@@ -2,6 +2,12 @@ package edu.vinicius.modulo.classe;
 
 public class MinhaClasse {
 
+    /*
+     * Uma classe é definida por atributos e metódos os atributos são variaveis de
+     * diferentes tipos e valores, os metodos correspondem a funções ou sub-rotinas
+     * dentro de nossas classes
+     */
+
     public static void main(String[] args) {
 
         String primeiroNome = "Vinicius";
---
+++ anatomia-classes/src/edu/vinicius/modulo/metodos/Metodos.java
@@ -0,0 +1,22 @@
+package edu.vinicius.modulo.metodos;
+
+public class Metodos {
+    /*
+     * Uma classe é definida por atributos e metódos os atributos são variaveis de
+     * diferentes tipos e valores, os metodos correspondem a funções ou sub-rotinas
+     * dentro de nossas classes
+     * 
+     * Alguns criterios para numeação de metodos:
+     * Deve ser nomeado como verbo
+     * Seguir o padrão camelCase
+     * Caso o metodo não retorne nada, devera ser declarado como void
+     * 
+     * Os metodos devem possuir apenas uma funcionalidade que deve condizer com o
+     * nome do metodo
+     * 
+     * Na assinatura do metodo temos o se é privado ou publico, tipo, nome do
+     * metodo, e parametros que se esperada
+     * 
+     * Vamos ver fizemos um exemplo com o programa SmartTV
+     */
+}
