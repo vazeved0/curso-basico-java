bb0649c771e2ba50f2b4dd52e4085c9c1ad2bd8c
Vinicius Azevedo <Viniciuslldeazevedo@gmail.com>
2022-12-05 20:47:02 -0300

    Palavras Reservadas

---
+++ anatomia-classes/src/edu/vinicius/modulo/palavrasReservadas/PalavrasReservadas.java
@@ -0,0 +1,66 @@
+package edu.vinicius.modulo.palavrasReservadas;
+
+public class PalavrasReservadas {
+    /*
+     * Palavras reservadas são identificadoes que ja possuem uma finalidade
+     * especeifica, não podendo ser utilizadas para nomear variaveis, classes,
+     * metodos ou atributos.
+     * 
+     * No java todas as palavras reservadas são escritas em letras minusculas, e
+     * atualmente o java possui 52 palavras reservadas.
+     * 
+     * Segue abaixo a lista de algumas das palavras reservadas agrupadas por sua
+     * finalidade
+     * 
+     * Controle de pacotes temos a seguintas palavras:
+     * - import: importa pacotes ou classes para dentro do código
+     * - package: identifica a qual pacote todas as classes dee um arquivo pertence
+     * 
+     * Modificadores de acessos:
+     * - public: acesso de qaulquer classe
+     * - private: acesso apenas dentro da classe
+     * - protected: acesso por classes no mesmo pacote e subclasses (herança)
+     * 
+     * Tipos primitivos
+     * - boolean: um valor indicando verdadeiro ou falso
+     * - byte: um inteiro de 8 bits(signed)
+     * - char: um character inicode (16-bit unsigned)
+     * - double: um numero de ponto flutuante de 64 bits (signed)
+     * - float: um numero de ponto flutuante de 32 bits (signed)
+     * - int: um numero inteiro de 32 bits (signed)
+     * - long: um inteiro de 64 bits (signed)
+     * - short: um inteiro de 32 bits (signed)
+     * - void: indica que o metodo não tem retorno de valor
+     * 
+     * Modificadores de Classes, variaveis ou metodos
+     * - abstract: classe que não pode ser instanciada ou metodo que precisa ser
+     * implementado por uma suclasse não abstrata
+     * - class: especifica uma classe
+     * - extends: Indica a superclasse que a subclasse esta estendendo
+     * - final: impossibilita que uma classe seja estendida, que um metodo seja
+     * sobrescrito ou que uma variavel seja reiniciada
+     * - implements: indica as interfaces que uma classe irá implementar
+     * - interface: especifica uma interface
+     * - native: indica que o metodo esta escrito em uma linguagem dependente de
+     * plataforma, omo o C
+     * - new: instancia um novo objeto, chamdo seu contrutor
+     * - static: faz um metodo ou variavel pertencer á classe ao invez de as
+     * instâncias
+     * - strictfp: usuado em frente a um metodo ou classe para identificar que os
+     * numeros de ponto flutuante seguirão as regras de ponto flutuante em todas as
+     * expressões
+     * - synchronized: indica que um metodo só pode ser acessado por um thread de
+     * cada vez
+     * - transient: impede a serialização de cmpos
+     * - volatile: indica que uma variavel pode ser alterada durante o uso de
+     * threads
+     * 
+     * Controle de fluxo dentro de um bloco de código
+     * - break: sai do cloco de codigo em que está
+     * - case: executa um bloco de codigo dependendo do teste do switch
+     * - continue: pula a execução do codigo que viria apos essa linha e vai para
+     * proxima passagem do loop
+     * 
+     */
+
+}
